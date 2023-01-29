b03b9c0bb40839673502d08d08d3767bff81fcf5
rotimi <rotimi@localhost>
2023-01-29 16:07:53 -0700

    Added \SlimMvcTools\Container which extends Pimple container & implements ContainerInterface.

---
+++ src/Container.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SlimMvcTools;
+
+use \Pimple\Container as PimpleContainer;
+use \Psr\Container\ContainerInterface;
+
+/**
+ * Description of Container
+ *
+ * @author rotimi
+ */
+class Container extends PimpleContainer implements ContainerInterface {
+
+    public function __construct(array $values = []) {
+        
+        parent::__construct($values);
+    }
+
+    public function get(string $id) {
+        
+        return $this->offsetGet($id);
+    }
+
+    public function has(string $id): bool {
+        
+        return $this->offsetExists($id);
+    }
+}
