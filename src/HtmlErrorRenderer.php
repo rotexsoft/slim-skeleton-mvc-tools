241d0d0de19728a739ed44245557dfdacde21745
rotimi <rotimi@localhost>
2023-02-02 00:27:25 -0700

    Slim 4 Prep

--- src/HtmlErrorRenderer.php
+++ src/HtmlErrorRenderer.php
@@ -10,11 +10,11 @@ namespace SlimMvcTools;
  */
 class HtmlErrorRenderer extends \Slim\Error\Renderers\HtmlErrorRenderer {
     
-    protected string $full_path_to_error_template_file = '';
+    protected string $path_to_error_template_file = '';
     
     public function __construct(string $full_path_to_error_template_file) {
         
-        $this->full_path_to_error_template_file = $full_path_to_error_template_file;
+        $this->path_to_error_template_file = $full_path_to_error_template_file;
     }
     
     public function __invoke(\Throwable $exception, bool $displayErrorDetails): string {
@@ -46,13 +46,13 @@ class HtmlErrorRenderer extends \Slim\Error\Renderers\HtmlErrorRenderer {
     
     public function renderHtmlBody(string $title = '', string $html = ''): string {
         
-        if($this->full_path_to_error_template_file === '') {
+        if($this->path_to_error_template_file === '') {
             
             return parent::renderHtmlBody($title, $html);
         }
         
         $file_contents = 
-            file_get_contents($this->full_path_to_error_template_file);
+            file_get_contents($this->path_to_error_template_file);
         
         return sprintf($file_contents, $title, $title, $html);
     }
