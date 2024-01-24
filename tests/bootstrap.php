181b7d5ece8fb2b8cc6ab88e7604b01436092b57
rotimi <rotimi@localhost>
2024-01-24 16:01:25 -0700

    Testing & refactoring in progress

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,38 +1,38 @@
 <?php
-    error_reporting(E_ALL | E_STRICT);
-    session_start();
+error_reporting(E_ALL | E_STRICT);
+session_start();
 
-    require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor/autoload.php';
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor/autoload.php';
 
-    if(!defined('SMVC_APP_ROOT_PATH')){
+if(!defined('SMVC_APP_ROOT_PATH')){
 
-        define('SMVC_APP_ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fake-smvc-app-root' );
+    define('SMVC_APP_ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fake-smvc-app-root' );
+}
+
+if(!defined('SMVC_APP_ENV_DEV')){
+
+    define('SMVC_APP_ENV_DEV', 'dev');
+}
+
+if(!defined('SMVC_APP_ENV_STAGING')){
+
+    define('SMVC_APP_ENV_STAGING', 'staging' );
+}
+
+if(!defined('SMVC_APP_ENV_TESTING')){
+
+    define('SMVC_APP_ENV_TESTING', 'testing' );
+}
+
+if(!defined('SMVC_APP_ENV_PRODUCTION')){
+
+    define('SMVC_APP_ENV_PRODUCTION', 'production' );
+}
+
+if(!function_exists('sMVC_GetCurrentAppEnvironment')) {
+
+    function sMVC_GetCurrentAppEnvironment(): string {
+
+        return sMVC_DoGetCurrentAppEnvironment(SMVC_APP_ROOT_PATH);
     }
-
-    if(!defined('SMVC_APP_ENV_DEV')){
-
-        define('SMVC_APP_ENV_DEV', 'dev');
-    }
-
-    if(!defined('SMVC_APP_ENV_STAGING')){
-
-        define('SMVC_APP_ENV_STAGING', 'staging' );
-    }
-
-    if(!defined('SMVC_APP_ENV_TESTING')){
-
-        define('SMVC_APP_ENV_TESTING', 'testing' );
-    }
-
-    if(!defined('SMVC_APP_ENV_PRODUCTION')){
-
-        define('SMVC_APP_ENV_PRODUCTION', 'production' );
-    }
-
-    if(!function_exists('sMVC_GetCurrentAppEnvironment')) {
-
-        function sMVC_GetCurrentAppEnvironment(): string {
-
-            return sMVC_DoGetCurrentAppEnvironment(SMVC_APP_ROOT_PATH);
-        }
-    }
+}
