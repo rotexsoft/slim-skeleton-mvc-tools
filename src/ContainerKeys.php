1a5aa2e80b42345f900b019102e3b3534ff197a7
rotimi <rotimi@localhost>
2024-01-31 19:23:05 -0700

    Testing, documentation & refactoring in progress

--- src/ContainerKeys.php
+++ src/ContainerKeys.php
@@ -12,6 +12,8 @@ namespace SlimMvcTools;
  */
 class ContainerKeys {
 
+    public const NEW_RESPONSE_OBJECT = 'new_response_object';
+    public const NEW_REQUEST_OBJECT = 'new_request_object';
     public const LAYOUT_RENDERER = 'new_layout_renderer';
     public const VIEW_RENDERER = 'new_view_renderer';
     public const LOGGER = 'logger';
