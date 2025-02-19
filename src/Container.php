fa5192f46a4be8baf662de54d5316821dcb2b7e8
rotimi <rotimi@localhost>
2025-02-19 14:08:22 -0700

    Psalm tweaks

--- src/Container.php
+++ src/Container.php
@@ -11,7 +11,6 @@ use \Psr\Container\ContainerInterface;
  *
  * @author rotimi
  * @psalm-suppress UnusedClass
- * @psalm-suppress ClassMustBeFinal
  */
 class Container extends PimpleContainer implements ContainerInterface {
 
